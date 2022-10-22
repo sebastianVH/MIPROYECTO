1c4889919f3aafda91b68cb0419149e6124e5670
Sebastian <sebastian.yacono@gmail.com>
2022-10-22 15:25:30 -0300

    Validaciones necesarias
    listo

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -43,11 +43,41 @@ $("#form_categoria").submit(function(){
         validacion.push("Ingrese el nombre de la categoria")
     }
     if (validacion.length >0){
-        alert("Se detectaron los siguientes errores:\n"+validacion.join("\n"))
+        alert("Se detectaron los siguientes errores:\n"+validacion.join("\n")+"\nSebastian Yacono")
         return false
     } else {
         return true;
     }
 })
 
+$("#form_productos").submit(function(){
 
+    var id_prod = $("#id_producto").val();
+    var nombre_prod = $("#nombre_producto").val();
+    var descripcion =$("#descripcion").val();
+    var precio_prod=$("#precio_producto").val();
+    var categoria=$("#categoria").val();
+    var validacion = [];
+
+    if ($.trim(id_prod) === ''){
+        validacion.push("Ingrese el ID del producto")
+    }
+    if ($.trim(nombre_prod) === ''){
+        validacion.push("Ingrese el nombre del producto")
+    }
+    if ($.trim(descripcion) === ''){
+        validacion.push("Ingrese una descripcion")
+    }
+    if ($.trim(precio_prod) === ''){
+        validacion.push("Ingrese un valor")
+    }
+    if ($.trim(categoria) === ''){
+        validacion.push("Seleccione una categoria")
+    }
+    if (validacion.length >0){
+        alert("Se detectaron los siguientes errores:\n"+validacion.join("\n")+"\nSebastian Yacono")
+        return false
+    } else {
+        return true;
+    }
+})
